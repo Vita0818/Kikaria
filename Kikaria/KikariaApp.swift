3edc830db303b05f9fc26934315d131d102b203d
Vita <qiji0818@outlook.com>
2026-05-01 15:40:07 +0800

    v0.1

---
+++ Kikaria/KikariaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KikariaApp.swift
+//  Kikaria
+//
+//  Created by Vita on 2026/5/1.
+//
+
+import SwiftUI
+
+@main
+struct KikariaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
